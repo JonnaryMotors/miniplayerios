a3faef32b93cdd1965d3d2ae6618a812ed7a803c
JonnaryMotors <60596122+JonnaryMotors@users.noreply.github.com>
2023-01-22 23:47:00 +0100

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,22 @@
+const podcasts = [
+    {
+      title: 'Lettera a un nemico',
+      artist: 'Serafina',
+      artwork: require('./img/2142935_0_idF_idE_500x500.jpg'),
+      url: 'https://api.spreaker.com/download/episode/43447400/letteraoreste.mp3',
+    },
+    {
+      title: '143 - Intelligenza Artificiale Generativa con Jacopo Perfetti',
+      artist: 'Hacking Creativity',
+      artwork: require('./img/108164_0_idF_idE_500x500.jpg'),
+      url: 'https://chtbl.com/track/9E947E/api.spreaker.com/download/episode/52096290/def_hc_perfetti_v2_loud.mp3',
+    },
+    {
+      title: 'IA : Microsoft investit 10 milliards dans Open AI pour tout dominer ?',
+      artist: 'Choses à savoir TECH',
+      artwork: require('./img/305627_0_idF_idE_500x500.jpg'),
+      url: 'https://traffic.megaphone.fm/FODL8281657475.mp3?updated=1673441802',
+    }
+  ];
+  
+  export default podcasts;
